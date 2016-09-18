2e65a4b5f348d916f425af4455cfd474325af0cc
Kallyn Gowdy <kallyngowdy@gmail.com>
2016-09-17 23:03:46 -0400

    Minor changes for packages

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,11 +1,11 @@
 import * as Angulators from './validators/';
 import * as Modifiers from './modifiers/';
-import {ValidationComponent, ValidationMessage} from './components';
-import {AngulatorsModule} from './angulators.module';
+import {ValidationComponent} from './components/validation-component/validation-component.component';
+// import {AngulatorsModule} from './angulators.module';
 
 export {
     Angulators,
     ValidationComponent,
-    ValidationMessage,
-    AngulatorsModule
+    // ValidationMessage,
+    // AngulatorsModule
 };
